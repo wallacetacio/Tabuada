55be1d6ef9448f5a6f8f4636bbf354a9b3592958
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-18 00:16:49 -0300

    Adiciona resolução ao exercício Tabuada ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fator1;
+            int fator2;
+            int produto;
+
+            Console.Write("Tabuada do número: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            fator1 = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+
+            fator2 = 0;
+
+            
+
+            while (fator2 <= 10)
+            {
+                produto = fator1 * fator2;
+
+                Console.WriteLine($"{fator1} x {fator2} = {produto} ");
+                fator2 = fator2 + 1;                 
+            }
+        }
+    }
+}
